9412f5c0b827586ed3beedf1da036eb7bd733396
Ajay ps <ajaypalamkunnel45@gmail.com>
2023-05-24 11:53:36 +0530

    name get thorugh url and it used using ternary operator

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   const router = useRouter();
   console.log(router.query);
+  const { name, age } = router.query;
   return (
-    
     <div>
-      
-      <h1>sum = {parseInt(router.query.num1)+parseInt(router.query.num2)}</h1>
-      <h1>Hello {router.query.name} Your age = {router.query.age}</h1>
+      {/* <h1>sum = {parseInt(num1)+parseInt(num2)}</h1> */}
+      <h1>{name ? `Hello , ${name}` : "I don't know your name"}</h1>
     </div>
   );
 }
